0c3c48d12b941bb2f5119a8b72a8c25eb5faf297
한승균 <sk.han@daumkakao.com>
2016-03-17 02:10:43 +0900

    Celeb과 SNS ID들이 many-to-one 관계를 가질 수 있도록 변경. * 하나의 ID를 여러 사람이 같이 쓰는 경우가 있음.

--- django_server/celebs/admin.py
+++ django_server/celebs/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Celeb, Tweet
+from .models import Celeb, TwitterAccount, FacebookAccount, InstagramAccount, Tweet
 
 # Register your models here.
 admin.site.register(Celeb)
+admin.site.register(TwitterAccount)
+admin.site.register(FacebookAccount)
+admin.site.register(InstagramAccount)
 admin.site.register(Tweet)
--- django_server/celebs/models.py
+++ django_server/celebs/models.py
@@ -7,14 +7,13 @@ class Celeb(models.Model):
     """한 명의 Celeb을 저장하는 model."""
     name = models.CharField(max_length=100) # celeb의 이름
     category = models.CharField(max_length=50, null=True, blank=True) # celeb이 속하는 카테고리. 가수, 배우, etc.
-
-    # each sns id.
-    twitter_id = models.CharField(max_length=50, null=True, blank=True)
-    facebook_id = models.CharField(max_length=50, null=True, blank=True)
-    instagram_id = models.CharField(max_length=50, null=True, blank=True)
-
     registered_time = models.DateTimeField(default=timezone.now()) # 등록 시각.
-    last_fetch_time = models.DateTimeField(null=True, blank=True) # 마지막으로 fetch한 시각.
+
+    # each sns account
+    # many-to-one relation.
+    twitter_account = models.ForeignKey('TwitterAccount', related_name='celebs', on_delete=models.SET_NULL, null=True, blank=True)
+    facebook_account = models.ForeignKey('FacebookAccount', related_name='celebs', on_delete=models.SET_NULL, null=True, blank=True)
+    instagram_account = models.ForeignKey('InstagramAccount', related_name='celebs', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.__unicode__().encode('utf8')
@@ -22,32 +21,54 @@ class Celeb(models.Model):
     def __unicode__(self):
         return u"{celeb.name} ({celeb.category})".format(celeb=self)
 
-    def get_twitter_url(self):
-        """twitter profile URL 반환"""
-        if self.twitter_id:
-            return "https://www.twitter.com/%s" % self.twitter_id
-        else:
-            return ""
 
-    def get_facebook_url(self):
-        """facebook profile URL 반환"""
-        if self.facebook_id:
-            return "https://www.facebook.com/%s" % self.facebook_id
-        else:
-            return ""
+class SnsAccount(models.Model):
+    """Sns Account를 나타내기 위한 클래스.
 
-    def get_instagram_url(self):
-        """instagram profile URL 반환"""
-        if self.instagram_id:
-            return "https://www.instagram.com/" % self.instagram_id
-        else:
-            return ""
+    아직 sub class에서 추가되는 필드는 없지만
+    실제 ID와 URL을 반환하기 위한 모습은 다를 수 있고
+    SNS마다 테이블을 따로 만들어야 하므로 abstract class로 구현한다"""
+
+    id = models.CharField(max_length=50, primary_key=True)
+    registered_time = models.DateTimeField(default=timezone.now()) # 등록 시각.
+    last_fetch_time = models.DateTimeField(null=True, blank=True) # 마지막으로 fetch한 시각.
+
+    def get_account_url(self):
+        raise NotImplementedError
+
+    class Meta:
+        abstract = True
+
+
+class TwitterAccount(SnsAccount):
+    def get_account_url(self):
+        return "https://www.twitter.com/%s" % self.id
+
+    def __unicode__(self):
+        return u"[twitter] %s" % self.id
+
+
+class FacebookAccount(SnsAccount):
+    def get_account_url(self):
+        return "https://www.facebook.com/%s" % self.id
+
+    def __unicode__(self):
+        return u"[facebook] %s" % self.id
+
+
+class InstagramAccount(SnsAccount):
+    def get_account_url(self):
+        return "https://www.instagram.com/" % self.id
+
+    def __unicode__(self):
+        return u"[instagram] %s" % self.id
 
 
 class Tweet(models.Model):
     """하나의 tweet을 저장하기 위한 모델."""
 
-    celeb = models.ForeignKey('celebs.Celeb', related_name='tweets') # 이 tweet을 쓴 사람
+    twitter_account = models.ForeignKey('TwitterAccount', related_name='tweets', on_delete=models.CASCADE) # 이 tweet을 account
+
     id = models.BigIntegerField(primary_key=True) # tweet id
     text = models.TextField() # tweet 내용
     created_at = models.DateTimeField() # tweet을 작성한 시각
@@ -60,4 +81,4 @@ class Tweet(models.Model):
         return "https://twitter.com/%s/status/%ld" % (self.celeb.twitter_id, self.id)
 
     def __unicode__(self):
-        return unicode(self.text)
+        return u"[{self.id}] {self.text}".format(self=self)
