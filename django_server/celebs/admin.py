4374549a92cc34b489cacf8e358e06d310a1b716
한승균 <sk.han@daumkakao.com>
2016-05-21 18:57:48 +0900

    celeb admin 추가.

--- django_server/celebs/admin.py
+++ django_server/celebs/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Celeb, TwitterAccount, FacebookAccount, InstagramAccount, Tweet, InstagramPost
 
+class CelebAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
 # Register your models here.
-admin.site.register(Celeb)
+admin.site.register(Celeb, CelebAdmin)
 admin.site.register(TwitterAccount)
 admin.site.register(FacebookAccount)
 admin.site.register(InstagramAccount)
