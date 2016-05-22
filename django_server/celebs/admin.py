c7fc98c8a5612c19d320f15c75df4876dc00d698
한승균 <sk.han@daumkakao.com>
2016-05-22 23:31:40 +0900

    admin site에 FacebookPost 등록.

--- django_server/celebs/admin.py
+++ django_server/celebs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Celeb, TwitterAccount, FacebookAccount, InstagramAccount, Tweet, InstagramPost
+from .models import *
 
 class CelebAdmin(admin.ModelAdmin):
     search_fields = ['name']
@@ -12,3 +12,4 @@ admin.site.register(FacebookAccount)
 admin.site.register(InstagramAccount)
 admin.site.register(Tweet)
 admin.site.register(InstagramPost)
+admin.site.register(FacebookPost)
